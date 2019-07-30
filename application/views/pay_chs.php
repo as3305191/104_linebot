948850bed810eb906812407adef7ae2b94e7d4b2
steve yeh <aapplle123@gmail.com>
2019-07-30 10:20:22 +0800

    push_2019-07-30.10:20:22

--- application/views/pay_chs.php
+++ application/views/pay_chs.php
@@ -31,7 +31,7 @@
 
 
 		<!-- #GOOGLE FONT -->
-		<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:400italic,700italic,300,400,700">
+		<link rel="stylesheet" href="//fonts.googleapis.com/css?family=Open+Sans:400italic,700italic,300,400,700">
 
 		<!-- #APP SCREEN / ICONS -->
 		<!-- Specifying a Webpage Icon for Web Clip
