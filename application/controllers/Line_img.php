a0207d5ab1165e1cfe8bb350285c8c506eadf7f0
江官駿 <as3305191@gmail.com>
2019-08-06 16:17:48 +0800

    push_2019-08-06.16:17:48

--- application/controllers/Line_img.php
+++ application/controllers/Line_img.php
@@ -605,7 +605,7 @@ class Line_img extends MY_Base_Controller {
 		return rand(pow(10, $digits - 1), pow(10, $digits) - 1);
 	}
 
-	public function line_result($last_id) {
+	public function line_result($last_id, $v, $size = 0) {
 		$bet = $this -> play_game_dao -> find_by_id($last_id);
 		$result=$bet->result;
 		$value = json_decode($result);
