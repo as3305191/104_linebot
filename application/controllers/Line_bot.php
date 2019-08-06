772426be8eea0fa4b99825101ac7feb936e7169b
jamie <jamie91200@gmail.com>
2019-08-06 16:47:37 +0800

    test

--- application/controllers/Line_bot.php
+++ application/controllers/Line_bot.php
@@ -785,7 +785,7 @@ class Line_bot extends MY_Base_Controller {
 	private function show_super_8_not_first(&$msg_arr,$id) {
 		$msg_arr[] = array(
 			"type" => "imagemap",
-			"baseUrl" => base_url("line_img/line_result/{$id}/v1/1"),
+			"baseUrl" => base_url("line_img/line_result/$id/v1/1"),
 			"altText" => "下注金額",
 			"baseSize" => array(
 				"width" => "1040",
