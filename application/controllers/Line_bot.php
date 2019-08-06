dab69f9e297c7fc41030333bbc54e18e9fbcd298
江官駿 <as3305191@gmail.com>
2019-08-06 17:03:51 +0800

    push_2019-08-06.17:03:51

--- application/controllers/Line_bot.php
+++ application/controllers/Line_bot.php
@@ -796,7 +796,7 @@ class Line_bot extends MY_Base_Controller {
 		$last_id=$id;
 		$msg_arr[] = array(
 			"type" => "imagemap",
-			"baseUrl" => base_url("line_img/line_result/{$last_id}/v1/1"),
+			"baseUrl" => base_url("line_img/line_jpg/first_game/v1/1"),
 			"altText" => "下注金額",
 			"baseSize" => array(
 				"width" => "1040",
@@ -805,7 +805,7 @@ class Line_bot extends MY_Base_Controller {
 			"actions" => array(
 				array(
 					"type" => "message",
-					"text" => "$last_id",
+					"text" => "881",
 					"area" => array(
 						"x" => 70,
 						"y" => 54,
