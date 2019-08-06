ab1b2da2b9257aaf5ff51545d1e4594a7e65e47b
江官駿 <as3305191@gmail.com>
2019-08-06 17:38:20 +0800

    push_2019-08-06.17:38:20

--- application/controllers/Line_bot.php
+++ application/controllers/Line_bot.php
@@ -732,64 +732,68 @@ class Line_bot extends MY_Base_Controller {
 					// 	"type" => "text",
 					// 	"text" => "$id",
 					// );
-						$this -> show_super_8_not_first($id);
+						$this -> show_super_8($id);
 
 				}
 			}
 		}
 	}
 
-	private function show_super_8(&$msg_arr, $is_first = FALSE) {
+	private function show_super_8($id) {
+		// $msg_arr[] = array(
+		// 	"type" => "imagemap",
+		// 	"baseUrl" => base_url("line_img/line_jpg/first_game/v1/1"),
+		// 	"altText" => "下注金額",
+		// 	"baseSize" => array(
+		// 		"width" => "1040",
+		// 		"height" => "1500"
+		// 	),
+		// 	"actions" => array(
+		// 		array(
+		// 			"type" => "message",
+		// 			"text" => "遊戲說明",
+		// 			"area" => array(
+		// 				"x" => 90,
+		// 				"y" => 845,
+		// 				"width" => 880,
+		// 				"height" => 202
+		// 			)
+		// 		),
+		// 		array(
+		// 			"type" => "message",
+		// 			"text" => "下注_超八_8",
+		// 			"area" => array(
+		// 				"x" => 90,
+		// 				"y" => 1234,
+		// 				"width" => 208,
+		// 				"height" => 202
+		// 			)
+		// 		),
+		// 		array(
+		// 			"type" => "message",
+		// 			"text" => "下注_超八_40",
+		// 			"area" => array(
+		// 				"x" => 425,
+		// 				"y" => 1234,
+		// 				"width" => 208,
+		// 				"height" => 202
+		// 			)
+		// 		),
+		// 		array(
+		// 			"type" => "message",
+		// 			"text" => "下注_超八_80",
+		// 			"area" => array(
+		// 				"x" => 758,
+		// 				"y" => 1234,
+		// 				"width" => 208,
+		// 				"height" => 202
+		// 			)
+		// 		),
+		// 	)
+		// );
 		$msg_arr[] = array(
-			"type" => "imagemap",
-			"baseUrl" => base_url("line_img/line_jpg/first_game/v1/1"),
-			"altText" => "下注金額",
-			"baseSize" => array(
-				"width" => "1040",
-				"height" => "1500"
-			),
-			"actions" => array(
-				array(
-					"type" => "message",
-					"text" => "遊戲說明",
-					"area" => array(
-						"x" => 90,
-						"y" => 845,
-						"width" => 880,
-						"height" => 202
-					)
-				),
-				array(
-					"type" => "message",
-					"text" => "下注_超八_8",
-					"area" => array(
-						"x" => 90,
-						"y" => 1234,
-						"width" => 208,
-						"height" => 202
-					)
-				),
-				array(
-					"type" => "message",
-					"text" => "下注_超八_40",
-					"area" => array(
-						"x" => 425,
-						"y" => 1234,
-						"width" => 208,
-						"height" => 202
-					)
-				),
-				array(
-					"type" => "message",
-					"text" => "下注_超八_80",
-					"area" => array(
-						"x" => 758,
-						"y" => 1234,
-						"width" => 208,
-						"height" => 202
-					)
-				),
-			)
+			"type" => "text",
+			"text" => "$id",
 		);
 	}
 
