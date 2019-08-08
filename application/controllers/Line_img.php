48664d222e917f7ad06e07291abd715c2ebe97ff
江官駿 <as3305191@gmail.com>
2019-08-08 10:30:49 +0800

    push_2019-08-08.10:30:49

--- application/controllers/Line_img.php
+++ application/controllers/Line_img.php
@@ -1283,7 +1283,7 @@ class Line_img extends MY_Base_Controller {
 		show_404();
 	}
 
-	public function line_gift($id , $v, $size = 0) {
+	public function line_gift($id , $v, $size) {
 		$download_file_name = HOME_DIR . "img/line688/line/wallet_card.jpg";
 		// header("Content-Disposition: attachment; filename=$img_name.jpg");
 		header("Content-type: image/jpeg");
