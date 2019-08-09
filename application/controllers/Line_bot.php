d5ae035b2929a782a3b751f2252307b834a06331
江官駿 <as3305191@gmail.com>
2019-08-09 10:23:01 +0800

    push_2019-08-09.10:23:01

--- application/controllers/Line_bot.php
+++ application/controllers/Line_bot.php
@@ -981,7 +981,7 @@ class Line_bot extends MY_Base_Controller {
 		$user = $this -> users_dao -> find_by_id($user_id);
 		$corp = $this -> corp_dao -> find_by_id(1);
 		$share_url = GAME_WEB_URL . "?promo={$user->gift_id}";
-		$line_share_url = urlencode("立即加入coc娛樂虛擬貨幣平台，讓你體驗漲的感覺及虛擬貨幣的娛樂應用" . GAME_WEB_URL . "?promo={$user->gift_id}");
+		$line_share_url = urlencode("立即加入coc娛樂虛擬貨幣平台，讓你體驗漲的感覺及虛擬貨幣的娛樂應用\n" . GAME_WEB_URL . "?promo={$user_id}");
 		$msg_arr[] = array(
 			"type" => "imagemap",
 			"baseUrl" => base_url("line_img/line_jpg/share/v2/1"),
