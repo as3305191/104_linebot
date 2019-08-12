4ec8bcb983fc5367948f28a11a971928cf4febe9
steve yeh <aapplle123@gmail.com>
2019-08-12 21:02:16 +0800

    push_2019-08-12.21:02:16

--- application/controllers/mgmt/Add_coin.php
+++ application/controllers/mgmt/Add_coin.php
@@ -128,11 +128,10 @@ class Add_coin extends MY_Mgmt_Controller {
 			$add_coin_daily=$this -> q_r_dao -> find_by_id($last_id_insert_q);
 
 			$dq =  $this -> d_q_dao -> find_d_q($Date);
-			$cp = floatval($add_coin_daily->current_point);
+			$cp = floatval($add_coin_daily->current_point); // 避免除0問題
 			$p = 0;
 			if($cp != 0) {
 				$p=floatval($add_coin_daily->current_ntd)/floatval($add_coin_daily->current_point);
-
 			}
 			$price1=round($p,8);
 			$dtx['date'] = $Date;
